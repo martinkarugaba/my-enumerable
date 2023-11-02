a57eb6fd51130049a19d61711f6b630724c3693b
Martin Karugaba <martinkarugaba21@gmail.com>
2023-11-02 22:42:28 +0300

    Remove tests.yml

--- verify.rb
+++ verify.rb
@@ -1,11 +1,8 @@
-# Load the modules and classes
 require_relative 'my_enumerable'
 require_relative 'my_list'
 
-# Sample data
 data = [1, 2, 3, 4, 5]
 
-# Create a MyList instance
 my_list = MyList.new(data)
 
 # Verify the functionality
